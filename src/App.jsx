54d34f174d90d3a380e4a224bf8a61c2a7aee6c2
Anna Lezhikova <anna.v.l@gmail.com>
2020-07-12 13:15:39 +1200

    shows result on word check

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import DICTIONARY from './dictionary';
 
 const check = (word, dictionary) => dictionary.includes(word);
 
 function App() {
-  const result = '';
+  const [result, setResult] = useState('');
+  const calcResult = (e) => {
+    e.preventDefault();
+    const result = check(e.target.testWord.value, DICTIONARY)
+      ? 'correct'
+      : 'incorrect';
+    setResult(result);
+  };
+
   return (
     <div className="App">
-      <form>
+      <form onSubmit={calcResult}>
         <label htmlFor="testWord">Type the word you want to check</label>
-        <input type="text" id="testWord" />
+        <input type="text" id="testWord" name="testWord" />
         <input type="submit" value="Check" />
       </form>
       <div>{result}</div>
